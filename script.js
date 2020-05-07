205681b1eaf0c9062659c1fa18ba8028e217d5b6
chanel <chaneldesirae18@gmail.com>
2020-05-07 17:17:18 -0500

    inital commit to start quiz hw

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const startButton = document.createElement('start-btn')
+
+startButton.addEventListener ('click', startButton) 
+   
+function startButton() {
+    console.log('Started')
+
+}
+
+
+function setNextQuesiton() {
+
+}
+
+function selectAnswer() {
+}
