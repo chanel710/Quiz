c83c9433fc3230d76386643c840871d229aa8323
chanel <chaneldesirae18@gmail.com>
2020-05-09 21:26:40 -0500

    submitting hw for quiz

--- script.js
+++ script.js
@@ -1,101 +1,71 @@
-///setup variables for Code Quiz   
-//When I click the start button
-var startBtn = document.querySelector("#start");
+//variables for quesstions 
+var questions = [
+    { q: "1. In Which HTML element do we put in JavaScript code?", a: "b", answers: ["body", "script", "link", "title"] },
+    { q: "2. Which of the following is not a data type of JavaScript?", a: "a", answers: ["function", "string", "boolean", "null"] },
+    { q: "3. Which answer describes the keyword This in JavaScript", a: "b", answers: ["answer a", "answer b", "answer c", "answer d"] },
+    { q: "4. Which option best describes the const vs. variable used in JavaScript?", a: "c", answers: ["answer a", "answer b", "answer c", "answer d"] },
+    { q: "5. Primative values can be used when writing code in JavaScript. Which one not correct?", a: "d", answers: ["answer a", "answer b", "answer c", "answer d"] },
+
+];
+//we start the game 
+var score = 0;
+var userAnswer = '';
+var currentquestionIndex = 0; // object 0, 1, 2, 3, 4 
+
+///setup variables for Code Quiz  reference for three containers 
+var startRef = document.getElementById("start-container");
+var questionRef = document.getElementById("question-container");
+var resultsRef = document.getElementById("results-container");
+var questiondisplayRef = document.getElementById("question");
+var answerbuttonsRef = document.getElementById("answer-buttons");
+var nextbuttonRef = document.getElementById("control-container");
+var answerInputs = answerbuttonsRef.getElementsByTagName("input"); //array of inputs 
+var answerLabels = answerbuttonsRef.getElementsByTagName("label"); // array of label for answers 
+
+
+
+function toggleDisplay(ref) {
+    startRef.style.display = "none";
+    questionRef.style.display = "none";
+    resultsRef.style.display = "none";
+    ref.style.display = "inline-block"; /// show will hide the rest 
+}
+//toggleDisplay(questionRef); // calling question ref only will show 
+//When I click the start button 
+var startBtn = document.querySelector("#start-btn");
 startBtn.addEventListener("click", function () {
-    Question();
-    start_timer();
+    console.log(startBtn);
+    loadQuestion(); // call function to load new questions  
+    toggleDisplay(questionRef);
 });
-///A timer starts 
-function start_timer() {
-    var timer = document.getElementById("myTimer").innerHTML;
-    var arr = timer.split(":");
-    var hour = arr[0];
-    var min = arr[1];
-    var sec = arr[2];
-    if (sec == 0) {
-        if (min == 0) {
-            if (hour == 0) {
-                alert("Times Up");
-                window.location.reload();
-            }
-            hour--;
-            min = 60;
-            if (hour < 10) hour = "0" + hour;
-        }
-        min--;
-        if (min < 10) min = "0" + min;
-        sec = c = 59;
-    } else sec--;
-    if (sec < 10) sec = "0" + sec;
-    document.getElementById("myTimer").innerHTML = hour + ":" + min + ":" + sec;
-    setTimeout(start_timer, 1000);
-}
-//Presented with a Question, When I answer a question, presented with another question/When I answer the question 
-//incorrectly then time is subtracted from the clock 
-//When all the questions are answered the timer reaches 0 
-//Then the game is over I can save my initals and score 
-var quesitons = '' [
 
-]
-
-
-class Questions {
-    constructor(text, options, result) {
-        this.text = text;
-        this.options = options;
-        this.result = result;
-    }
-}
-
-Question.prototype.correctAnswer = function(option) {
-    return option === this.result;
-}
-
-function populate () {
-    if (quiz.isEnded() ) {
-    }
-    else {
-        //show questions 
-        var element = document.getElementById("question");
-        element.innerHTML = quiz.getQuestionIndex().text;
-        //choices
-        var options = quiz.getQuestionIndex().options;
-        for(var i = 0; i< choices.length; i++) {
-            var element = docment.getElementbyId("options" + i);
-            element.innerHTML = options[i];
-        }
+{
+    var nextBtn = document.querySelector("#next-btn");
+    nextBtn.addEventListener("click", function () {
+    console.log(nextBtn);
+    alert("next questions");
+    loadQuestion();
+    toggleDisplay(nextbuttonRef);
+    });
     
-        class Quiz {
-            constructor(questions) {
-                this.score = 0;
-                this.questions = quesitons;
-                this.getQuestionIndex = 0;
-            }
-            isEnded() {
-                return this.quesitons.length === this.questions.Index;
-            }
-        }
-
-Quiz.prototype.getQuestionIndex = function() {
-    return this.quesitons[this.getQuestionIndex]
-} 
-
-
-Quiz.prototype.guess = funciton(answer) 
-    this.questionIndex++;
-
-    if(this.getQuestionIndex().correctResult(result)) {
-
-    }
 }
 
+function loadQuestion() {
+    var currentQuestion = questions[currentquestionIndex];
+    //var answerContainers = startContainer.querySelectorAll('.answers');
+    questiondisplayRef.textContent = currentQuestion.q;   //accessing question of 0 
+    for (var i = 0; i < currentQuestion.answers.length; i++) { //for loop all answers within question
+        answerLabels[i].textContent = currentQuestion.answers[i]; // mirrored the same labels to questions/answers 
+    }
+    
+    }
+    
 
 
 
 
 
 
-//setup Event listners for buttons 
-//You are required to submit the following for review:
-// The URL of the functional, deployed application.
-//The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+
+
